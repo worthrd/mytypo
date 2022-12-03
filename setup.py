73b4b69407123430a3f51f86d8dd67a10d9b5e62
Recep Daban <recep.daban@gmail.com>
2022-12-03 14:34:02 +0300

    organize folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="mytypo",
+    version="0.0.1",
+    description="Checks typos in one lines comments",
+    py_modules=["mytypo"],
+    package_dir={'':'src'},
+)
--- tests/test_mytypo.py
+++ tests/test_mytypo.py
@@ -1,10 +1,10 @@
 import pytest
 import os
 
-from ..mytypo import check_typo
+from src.mytypo import check_typo
 # this comment has miss spel word
 
 def test_typo_correction():
-    p = os.path.abspath(".") + "/mytypo/tests/test_mytypo.py"
+    p = os.path.abspath(".") + "/tests/test_mytypo.py"
     suggestions = check_typo(path=p)
     assert len(suggestions) == 1
